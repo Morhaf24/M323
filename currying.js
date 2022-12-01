bdd23b53397134e0e0a47267aba85de7cc2d548d
DESKTOP-CA512TH\Morha <morhaf.alnhlawe@gmail.com>
2022-12-01 07:53:43 +0100

    I changed the Rambda, becuase it just got an Error

--- currying.js
+++ currying.js
@@ -14,20 +14,30 @@ const studentFeedback = [
     'Not good Gina, you got an f',
   ]; 
 
-  const greet = (studentFeedback) => {
-    function greetName(studentGrades) {
-        if (studentGrades >= 90) return `Excellent Job Jen, you got an a
-        ${studentGrades.name}`;
-        if (studentGrades >= 80) return `Nice Job Joe, you got an b
-        ${studentGrades.name}`;
-        if (studentGrades >= 70) return `Well done Steph, you got an c
-        ${studentGrades.name}`;
-        if (studentGrades >= 60) return `What happend Allen, you got an d
-        ${studentGrades.name}`;
-        return `Not good Gine, you got an f ${studentGrades.name}`;
-    }
-    return greetName;
-  };
+const greet = (studentFeedback) => {
+  function greetName(studentGrades) {
+      if (studentGrades >= 90) return `Excellent Job Jen, you got an a
+      ${studentGrades.name}`;
+      if (studentGrades >= 80) return `Nice Job Joe, you got an b
+      ${studentGrades.name}`;
+      if (studentGrades >= 70) return `Well done Steph, you got an c
+      ${studentGrades.name}`;
+      if (studentGrades >= 60) return `What happend Allen, you got an d
+      ${studentGrades.name}`;
+      return `Not good Gine, you got an f ${studentGrades.name}`;
+  }
+  return greetName;
+};
 
-  console.log(studentGrades.map(greet(studentFeedback)));
+console.log(studentGrades.map(greet(studentFeedback)));
   
+
+const req = require('./rambda.min.js');
+
+const nameArray = ["Morhaf", "Mouayad", "Anna"];
+
+const halloFunction = req.curry((prefix, item) => prefix + item);
+const halloName = req.map(halloFunction('Hallo, geht es dir gut? ' ),
+nameArray);
+
+console.log(halloName);
--- primitive_datatypes.js
+++
@@ -1,8 +0,0 @@
-const persons = [
-    {name: "Morhaf", age: 20 },
-    {name: "Morhaf", age: 20 },
-    5,
-    true,
-]
-
-persons.push({name, "Anna", age: 18});
